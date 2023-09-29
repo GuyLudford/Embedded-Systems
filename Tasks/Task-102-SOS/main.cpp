1aa1375670855e34fa78cd405a958420fc8fba2e
GuyLudford <71602999+GuyLudford@users.noreply.github.com>
2023-09-29 14:16:34 +0100

    thing

--- Tasks/Task-102-SOS/main.cpp
+++ Tasks/Task-102-SOS/main.cpp
@@ -1,39 +1,22 @@
-// You need this to use the Module Support Board
 #include "../lib/uopmsb/uop_msb_2_0_0.h"
+#include "BMP280_SPI.h"
+
 using namespace uop_msb_200;
 
-#define WAIT_TIME_MS 500 
-DigitalOut greenLED(TRAF_GRN1_PIN);
-Buzzer buzz;
-Buttons buttons;
+LCD_16X2_DISPLAY lcd;
 
-// TIP: (I suggest you read this!)
-//
-// Press the black reset button to restart the code (and stop the sound)
-// Otherwise, the noise can be "distracting" :)
+extern EnvironmentalSensor sensor;
 
-int main()
-{
-    //Wait for the BLUE button to be pressed (otherwise this becomes super annoying!)
-    while (buttons.BlueButton == 0);
-    
-    //Repeat everything "forever" (until the power is removed or the chip is reset)
-    while (true)
-    {
-        //On for 500ms
-        greenLED = 1;
-        buzz.playTone("C");
-        wait_us(WAIT_TIME_MS * 1000);  //500ms
+float pressure;
 
+int main(){
 
-        //Off for 500ms
-        greenLED = 0;
-        buzz.playTone("C", Buzzer::HIGHER_OCTAVE);
-        wait_us(WAIT_TIME_MS * 1000);  //500ms
-
-        //Pause
-        buzz.rest();
-        wait_us(WAIT_TIME_MS * 1000);
+    while (true){
+        lcd.cls();
+        pressure  = sensor.getPressure();
+        lcd.printf("pressure = %.1fmBar\n", pressure);
+        wait_us(100000);
 
     }
-}
+
+} 
